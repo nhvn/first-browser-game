fe90275aefae2a476926720b430422feec8e0be7
Alan <alannhan@ymail.com>
2023-05-16 17:25:09 -0700

    added chaser but not moving at right speed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Background
+// BACKGROUND
 const background = (document.querySelector(".myImg").src = "./img/4_game_background.png");
 const platform = (document.querySelector(".myImg").src = "./img/platform.png");
 const spider = (document.querySelector(".myImg").src = "./img/Spider2.png");
@@ -7,21 +7,23 @@ const pumpkin = (document.querySelector(".myImg").src = "./img/Pumpkin3.png");
 const findpumpkin = (document.querySelector(".myImg").src = "./img/findpumpkin.png");
 const sparkle = (document.querySelector(".myImg").src = "./img/sparkle.png");
 const canvas = document.querySelector("canvas");
+const chaser = (document.querySelector(".myImg").src = "./img/chaser.png");
 const c = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Sprites
+// SPRITE
 const spriteStandRight = (document.querySelector(".myImg").src = "./img/idleRight.png");
 const spriteRunRight = (document.querySelector(".myImg").src = "./img/walkRight.png");
 const spriteRunLeft = (document.querySelector(".myImg").src = "./img/walkLeft.png");
-const spriteStandLeft = (document.querySelector(".myImg").src = "./img/idleRight.png");
+const spriteStandLeft = (document.querySelector(".myImg").src = "./img/idleLeft.png");
 
 const gravity = 0.2; // Gravity (similar to line 270 and 286)
+
+// PROPERTIES OF CHARACTER
 class Player {
   constructor() {
-    // Properties associated with player
     this.speed = 5;
     this.position = { // Starting position
       x: 30,
@@ -42,7 +44,7 @@ class Player {
         left: createImage(spriteStandLeft)
       },
       run: {
-        right: createImage(spriteRunRight),
+        right: createImage(spriteRunRight), // Displays running left & right
         left: createImage(spriteRunLeft)
       }
     };
@@ -52,7 +54,7 @@ class Player {
   draw() {
     c.drawImage(
       this.currentSprite,
-      32 * this.frames, // Adds Idle bouncing (but too fast)
+      32 * this.frames,
       0,
       32, // Adds player width from image
       32, // Adds player height from image
@@ -73,19 +75,19 @@ class Player {
     }
   
     if (this.frames > 1 && this.currentSprite === this.sprites.stand.right)
-      this.frames = 0; // Adds Idle bouncing (but too fast) pt.2
+      this.frames = 0; 
     else if (this.frames > 4 && this.currentSprite === this.sprites.run.right)
       this.frames = 0;
   
     this.draw();
-    this.position.y += this.velocity.y; // These 2 are used to be added to for control movement
+    this.position.y += this.velocity.y; // Added to for control movement
     this.position.x += this.velocity.x;
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) // This adds a ground to stop velocity
-      this.velocity.y += gravity; // This adds velocity to gravity
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) // Ground to stop velocity
+      this.velocity.y += gravity; // Adds velocity to gravity
   }
-  
 }
 
+// PLATFORM & GENERAL OBJECT
 class Platform {
   constructor({ x, y, image }) {
     this.position = {
@@ -97,13 +99,11 @@ class Platform {
     this.height = image.height;
   }
   draw() {
-    // Draws rectangle
     c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
 class GenericObject {
-  // This is for objects that we can walk by
   constructor({ x, y, image }) {
     this.position = {
       x,
@@ -114,13 +114,11 @@ class GenericObject {
     this.height = image.height;
   }
   draw() {
-    // Draws rectangle
     c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
 function createImage(imageSrc) {
-  // This code allows creation for new images rather than making a new const everytime
   const image = new Image();
   image.src = imageSrc;
   return image;
@@ -142,10 +140,12 @@ const keys = {
 
 let scrollOffset = 0;
 
+let chaserDistance = 0; // New variable to track chaser's relative movement
+
 function init() {
   platformImage = createImage(platform);
-
   player = new Player();
+
   platforms = [
     new Platform({ x: 0, y: 470, image: platformImage }), // Sets up first platform
     new Platform({ x: 700, y: -300, image: platformImage }),
@@ -180,14 +180,51 @@ function init() {
     new GenericObject({ x: 7650, y: 370, image: createImage(pumpkin) }),
     new GenericObject({ x: 200, y: 300, image: createImage(findpumpkin) }),
   ];
+
+  // CHASER 1
+  movingObject = new MovingObject({
+    x: -100,  // Initial x position outside the canvas on the left
+    y: 0,     // Starting y position
+    image: createImage(chaser),
+    speed: 1, // Adjust the speed as needed
+  });
   
-
   scrollOffset = 0;
-    keys.right.pressed = false;
-
+  keys.right.pressed = false;
+  chaserDistance = 0;
 }
 
-// Allows to maintain character shape
+// CHASER 2
+class MovingObject {
+  constructor({ x, y, image, speed }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = canvas.height; // Cover the entire height of the canvas
+    this.speed = speed;
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.position.x += this.speed; // Move towards the right at a fixed speed
+  }  
+}
+
+let movingObjectImage = createImage(chaser);
+let movingObject = new MovingObject({
+  x: canvas.width,
+  y: 420,
+  image: movingObjectImage,
+  speed: 1,
+});
+
+// ALLOWS TO MAINTAIN SHAPE
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "white";
@@ -199,7 +236,17 @@ function animate() {
   platforms.forEach((platform) => {
     platform.draw();
   });
+
   player.update();
+  movingObject.update();
+  movingObject.draw();
+
+  // CHASER 3
+  if (keys.right.pressed) {
+    movingObject.position.x -= player.speed;
+  } else if (keys.left.pressed && scrollOffset > 0) {
+    movingObject.position.x -= player.speed;
+  }
 
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = player.speed;
@@ -213,7 +260,7 @@ function animate() {
     if (keys.right.pressed) {
       scrollOffset += player.speed;
       platforms.forEach((platform) => {
-        platform.position.x -= player.speed; // Allows platform to move as same rate as character
+        platform.position.x -= player.speed;
       });
       genericObjects.forEach((genericObject) => {
         genericObject.position.x -= player.speed * 0.66;
@@ -221,16 +268,24 @@ function animate() {
     } else if (keys.left.pressed && scrollOffset > 0) {
       scrollOffset -= player.speed;
       platforms.forEach((platform) => {
-        platform.position.x += player.speed; // Same as above but for left side
+        platform.position.x += player.speed;
       });
 
       genericObjects.forEach((genericObject) => {
         genericObject.position.x += player.speed * 0.66;
       });
     }
+    if (scrollOffset > 11000) {
+      alert("You Win!");
+      init();
+    }
+  
+    if (player.position.y > canvas.height || player.position.y < -50) {
+      init();
+    }
   }
 
-  // Platform collision detection
+  // PLATFORM COLLISION DETECTION
   platforms.forEach((platform) => {
     if (
       player.position.y + player.height <= platform.position.y && // Allows to stay onto platform 
@@ -240,7 +295,7 @@ function animate() {
     ) {
       player.velocity.y = 0; // Allows you you to stand on platforms
     }
-    else if ( // Successfully created horizontal barriers which took about 10+ hours of trial and error
+    else if ( // HORIZONTAL BARRIERS
       player.position.x + player.width >= platform.position.x &&
       player.position.x <= platform.position.x + platform.width &&
       player.position.y + player.height >= platform.position.y &&
@@ -251,13 +306,24 @@ function animate() {
     }
   })
 
-  // Win condition
+  // MOVING OBJECT COLLISION DETECTION
+  if (
+    player.position.x < movingObject.position.x + movingObject.width &&
+    player.position.x + player.width > movingObject.position.x &&
+    player.position.y < movingObject.position.y + movingObject.height &&
+    player.position.y + player.height > movingObject.position.y
+  ) {
+    // collision detected!  
+    init();
+  }
+
+  // WIN CONDITION
   if (scrollOffset > 11000) {
     alert("You Win!");
     init()
   }
 
-  // Lose condition
+  // LOSE CONDITION
   if (
     player.position.y > canvas.height ||
     player.position.y < -50
@@ -269,16 +335,20 @@ function animate() {
 init();
 animate();
 
-  // This creates character movement
+// CHARACTER MOVEMENT
 addEventListener("keydown", ({ keyCode }) => {
   switch (keyCode) {
-    case 68:
+    case 68: // d key
       console.log("right");
       keys.right.pressed = true;
       player.currentSprite = player.sprites.run.right;
       break;
-
-    case 87:
+    case 65: // a key
+      console.log("left");
+      keys.left.pressed = true;
+      player.currentSprite = player.sprites.run.left;
+      break;
+    case 87: // w key
       console.log("up");
       player.velocity.y -= 0.5; // How high character jumps
       break;
@@ -288,15 +358,20 @@ addEventListener("keydown", ({ keyCode }) => {
 addEventListener("keyup", ({ keyCode }) => {
   // This allows endless gravity to stop
   switch (keyCode) {
-    case 68:
+    case 68: // d key
       console.log("right");
       keys.right.pressed = false;
       player.currentSprite = player.sprites.stand.right;
       break;
-
-    case 87:
+    case 65: // a key
+      console.log("left");
+      keys.left.pressed = false;
+      player.currentSprite = player.sprites.stand.left;
+      break;
+    case 87: // w key
       console.log("up");
       player.velocity.y -= 5;
       break;
   }
 });
+
